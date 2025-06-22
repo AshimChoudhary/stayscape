824d1c62e8923aa8d9d2f8833b81dacb9c92f763
Ashim <panghalashim7832@gmail.com>
2025-06-22 16:05:34 +0530

    Auth Phase III

--- app/components/modals/RegisterModel.tsx
+++ app/components/modals/RegisterModel.tsx
@@ -5,8 +5,41 @@ import { useCallback, useState } from 'react';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import useRegister from '@/app/hooks/useRegisterModel';
+import { register } from 'module';
+import { error, log } from 'console';
 
 const RegisterModel = () => {
+  const registerModel = useRegister();
+  const [oading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+
+    axios
+      .post('/api/register', data)
+      .then(() => {
+        registerModel.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return <div>RegisterModel</div>;
 };
 
