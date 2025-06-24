8dc0210387634fe83a5b68a50032ce951db936e3
Ashim <panghalashim7832@gmail.com>
2025-06-24 22:11:05 +0530

    H

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -2,6 +2,9 @@
 
 import { TbBeach } from 'react-icons/tb';
 import Components from '../Components';
+import { GiWindmill } from 'react-icons/gi';
+import { MdOutlineVilla } from 'react-icons/md';
+import CategoryBox from '../CategoryBox';
 
 export const categories = [
   {
@@ -10,15 +13,29 @@ export const categories = [
     description: 'This Property is close to the beach',
   },
   {
-    label: 'Beach',
-    icon: TbBeach,
-    description: 'This Property is close to the beach',
+    label: 'WindMills',
+    icon: GiWindmill,
+    description: 'This Property has windmills',
+  },
+  {
+    label: 'Modern',
+    icon: MdOutlineVilla,
+    description: 'This Property is Modern!!',
   },
 ];
 const Categories = () => {
   return (
     <Components>
-      <div className="pt-4 flex flex-row items-center justify-between overflow-x-autp"></div>
+      <div className="pt-4 flex flex-row items-center justify-between overflow-x-autp">
+        {categories.map((item) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            description={item.description}
+            icon={item.icon}
+          />
+        ))}
+      </div>
     </Components>
   );
 };
