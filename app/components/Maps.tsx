147c66b6db521c0a080a9d5eaa0fe0a39417280f
Ashim <panghalashim7832@gmail.com>
2025-06-25 13:08:52 +0530

    D

--- app/components/Maps.tsx
+++ app/components/Maps.tsx
@@ -1,7 +1,32 @@
 'use client';
+import L from 'leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import { FC } from 'react';
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
+});
 
-const Maps = () => {
-  return <div>Maps</div>;
+interface MapProps {
+  center?: number[];
+}
+
+const Maps: FC<MapProps> = ({ center }) => {
+  return (
+    <MapContainer
+      center={(center as L.LatLngExpression) || [51, 23]}
+      zoom={center ? 4 : 2}
+      scrollWheelZoom={false}
+      className="h-[35vh] rounded-lg  "
+    ></MapContainer>
+  );
 };
 
 export default Maps;
